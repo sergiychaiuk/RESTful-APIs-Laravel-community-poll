5c33d4ef3bc7a2fbeed01d89681c84cd561e5f8a
serhiichayck <chayck7@gmail.com>
2021-04-05 21:28:50 +0300

    Connecting project to a database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+//        factory(\App\Models\User::class, 5)->create();
+//        factory(\App\Models\Poll::class, 10)->create();
+//        factory(\App\Models\Question::class, 50)->create();
+//        factory(\App\Models\Answer::class, 500)->create();
+        \App\Models\User::factory(5)->create();
+        \App\Models\Poll::factory(10)->create();
+        \App\Models\Question::factory(50)->create();
+        \App\Models\Answer::factory(500)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
