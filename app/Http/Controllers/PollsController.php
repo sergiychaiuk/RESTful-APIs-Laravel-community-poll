1ee5b69fcf0014adf9db39b9868bee71968e0502
serhiichayck <chayck7@gmail.com>
2021-04-06 20:05:50 +0300

    Returning validation errors

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Poll;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PollsController extends Controller
 {
@@ -23,6 +24,13 @@ class PollsController extends Controller
 
     public function store(Request $request)
     {
+        $rules = [
+            'title' => 'required|max:255',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
         $poll = Poll::create($request->all());
         return response()->json($poll, 201);
     }
