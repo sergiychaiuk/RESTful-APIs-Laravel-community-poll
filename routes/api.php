a99e779203d3db6a3e2c3c0c08bdd44aebfa7d98
serhiichayck <chayck7@gmail.com>
2021-04-07 17:13:16 +0300

    Accepting file uploads

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ Route::any('errors', 'PollsController@errors');
 Route::apiResource('questions', 'QuestionsController');
 Route::get('polls/{poll}/questions', 'PollsController@questions');
 Route::get('files/get', 'FilesController@show');
+Route::post('files/create', 'FilesController@create');
